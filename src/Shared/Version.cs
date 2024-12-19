9d130b519247fa97c638080a61c578e578eefdbc
Uladzislau Nikalayevich <thenormalnij@gmail.com>
2024-12-19 17:53:55 +0300

    Bump version

--- src/Shared/Version.cs
+++ src/Shared/Version.cs
@@ -3,7 +3,7 @@ namespace MVirus.Shared
 {
     public class Version
     {
-        public static uint VERSION = 1;
-        public static uint MINIMAL_CLIENT_VERSION = 1;
+        public static uint VERSION = 2;
+        public static uint MINIMAL_CLIENT_VERSION = 2;
     }
 }
