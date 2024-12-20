b7b1ae47105ccc663545bed29d0020088ea6ee48
Uladzislau Nikalayevich <thenormalnij@gmail.com>
2024-12-20 23:35:25 +0300

    Increase cache scan speed. First iteration

--- src/Client/CacheScanner.cs
+++ src/Client/CacheScanner.cs
@@ -2,39 +2,103 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MVirus.Client
 {
     public class CacheScanner
     {
-        public static async Task<List<ServerFileInfo>> FilterLocalFiles(List<ServerFileInfo> files, string cachePath, Action<ServerFileInfo> exists)
+
+        public async static Task<List<ServerFileInfo>> FilterLocalFiles(List<ServerFileInfo> files, string cachePath, Action<ServerFileInfo> exists)
         {
-            var filteredList = new List<ServerFileInfo>();
+            if (files.Count == 0)
+                return new List<ServerFileInfo>();
 
-            foreach (var fileInfo in files)
+            return await FilterLocalFilesThisThreadAsync(files, cachePath, exists);
+        }
+
+        private static async Task<List<ServerFileInfo>> FilterLocalFilesThisThreadAsync(List<ServerFileInfo> files, string cachePath, Action<ServerFileInfo> exists)
+        {
+
+            var tasksArray = files.ToArray();
+            var index = -1;
+
+            ServerFileInfo getNextCheck()
             {
-                var targetPath = Path.Combine(cachePath, fileInfo.Path);
+                index++;
+                if (tasksArray.Length > index)
+                    return tasksArray[index];
+                return null;
+            }
 
-                try
-                {
-                    var crc32 = await Crc32.CalculateFileCrc32Async(targetPath);
-                    if (crc32.Equals(fileInfo.Crc))
-                        exists.Invoke(fileInfo);
-                    else
-                        filteredList.Add(fileInfo);
-                }
-                catch (FileNotFoundException _)
-                {
-                    filteredList.Add(fileInfo);
-                }
-                catch (DirectoryNotFoundException _)
-                {
-                    filteredList.Add(fileInfo);
-                }
+            CheckTaskInfo getNextTask()
+            {
+                var check = getNextCheck();
+                if (check == null)
+                    return null;
+
+                Log.Warning(check.Path);
+                var path = Path.Combine(cachePath, check.Path);
+                return new CheckTaskInfo{ task = IsFileCached(path, check.Crc), data = check };
+            }
+
+            List<CheckTaskInfo> currentTasks = new List<CheckTaskInfo>();
+            for (int i = 0; i < 3; i++)
+            {
+                var task = getNextTask();
+                if (task == null)
+                    break;
+
+                currentTasks.Add(task);
+            }
+
+            var filteredList = new List<ServerFileInfo>();
+            for (; ; )
+            {
+                var finished = await Task.WhenAny(currentTasks.Select( item => item.task ));
+
+                var taskItem = currentTasks.Find(item => item.task == finished);
+
+                if (finished.Result)
+                    exists.Invoke(taskItem.data);
+                else
+                    filteredList.Add(taskItem.data);
+
+                currentTasks.Remove(taskItem);
+
+                var nextTask = getNextTask();
+                if (nextTask != null)
+                    currentTasks.Add(nextTask);
+
+                if (currentTasks.Count == 0)
+                    break;
             }
 
             return filteredList;
+        }
+
+        public static async Task<bool> IsFileCached(string targetPath, int expectedCrc)
+        {
+            try
+            {
+                var crc32 = await Crc32.CalculateFileCrc32Async(targetPath);
+                return crc32.Equals(expectedCrc);
+            }
+            catch (FileNotFoundException)
+            {
+                return false;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return false;
+            }
+        }
+
+        private class CheckTaskInfo
+        {
+            public Task<bool> task;
+            public ServerFileInfo data;
         }
     }
 }
