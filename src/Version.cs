0fa790c67bd850100878c696b8bb5cb1c002bb5a
Uladzislau Nikalayevich <thenormalnij@gmail.com>
2025-01-20 20:21:38 +0300

    Hardcode version number

--- src/Version.cs
+++ src/Version.cs
@@ -3,7 +3,7 @@ namespace MVirus
 {
     public class Version
     {
-        public static System.Version ModVersion => API.instance.Version;
-        public static readonly System.Version MinimalClientVersion = System.Version.Parse("1.0.0");
+        public static System.Version ModVersion => System.Version.Parse("0.4.0");
+        public static readonly System.Version MinimalClientVersion = System.Version.Parse("0.4.0");
     }
 }
